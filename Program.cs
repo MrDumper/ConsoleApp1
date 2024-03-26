fdb9f03aac64342ab88a60fe847085cfe4cc1e7f
MrDumper <romochka-mironyuk@mail.ru>
2024-03-26 12:28:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!!!"); // Метод WriteLine переносит курсор на новую строку после выполнения
+Console.Write("Hello, "); // Метод Write оставляет курсор на том же месте, после выполнения
+Console.Write("World");
+Console.Write("!!!");
+Console.ReadKey(); // Метод ReadKey ожидает нажатия какой-либо клавиши
